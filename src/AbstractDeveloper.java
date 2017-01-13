c4e5b79d2586c33a4173bf7edf77738b55f73845
Rob <rold@fleetsmart.co.uk>
2017-01-13 08:42:08 +0000

    Initial commit

---
+++ src/AbstractDeveloper.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Rob on 13/01/2017.
+ */
+public  class AbstractDeveloper {
+    private String name;
+    private String favouriteSong;
+    private String favouriteBiscuit;
+
+
+}
